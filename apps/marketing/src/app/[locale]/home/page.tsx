e33376902a324718fc4943f058dadf8c886dfba9
Henry <>
2025-01-08 23:22:03 +0800

    [feat] Add Hero to home

--- apps/marketing/src/app/[locale]/home/page.tsx
+++ apps/marketing/src/app/[locale]/home/page.tsx
@@ -1,12 +1,5 @@
-import { Button } from "@pt/ui/button";
-import { useTranslations } from "next-intl";
+import { Hero } from "@/components/home/Hero";
 
 export default function Home() {
-  const t = useTranslations();
-  return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <h1>{t("echo.hello")}</h1>
-      <p>{t("echo.welcome")}</p>
-    </div>
-  );
+  return <Hero />;
 }
