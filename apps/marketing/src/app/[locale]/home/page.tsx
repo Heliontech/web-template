473eae7e1abd4f44549eed80e6192cdcbd766e36
Henry <>
2025-01-17 23:42:29 +0800

    [feat] add features section

--- apps/marketing/src/app/[locale]/home/page.tsx
+++ apps/marketing/src/app/[locale]/home/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/components/home/Hero";
 import { FAQ } from "@/components/home/FAQ";
+import { Features } from "@/components/home/Features";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Features />
       <FAQ />
     </>
   );
