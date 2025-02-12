a43b8c7589e42b9b0dde6ac7ed3fe27858e75e4b
Henry <>
2025-02-12 14:22:29 +0800

    [change] separate the search parameter with nuqs

--- apps/admin/lib/searchparams.ts
+++ apps/admin/lib/searchparams.ts
@@ -5,13 +5,30 @@ import {
   parseAsString
 } from 'nuqs/server';
 
-export const searchParams = {
+export const commonSearchParams = {
   page: parseAsInteger.withDefault(1),
   limit: parseAsInteger.withDefault(10),
-  q: parseAsString,
-  gender: parseAsString,
-  categories: parseAsString
+  q: parseAsString
 };
 
-export const searchParamsCache = createSearchParamsCache(searchParams);
-export const serialize = createSerializer(searchParams);
+// User search params
+export const userSearchParams = {
+  ...commonSearchParams,
+  gender: parseAsString,
+  status: parseAsString
+};
+
+export const userSearchParamsCache = createSearchParamsCache(userSearchParams);
+export const userSearchParamsSerializer = createSerializer(userSearchParams);
+
+// Product search params
+export const productSearchParams = {
+  ...commonSearchParams,
+  categories: parseAsString,
+  status: parseAsString
+};
+
+export const productSearchParamsCache =
+  createSearchParamsCache(productSearchParams);
+export const productSearchParamsSerializer =
+  createSerializer(productSearchParams);
